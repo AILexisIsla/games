58ed108fda79602effbe9a3217c14537fbdb8f03
AlexisIsla <alexis.u.isla@gmail.com>
2023-10-11 02:49:42 -0300

    add itemDetail js code redirect onclick img

--- assets/js/itemDetail.js
+++ assets/js/itemDetail.js
@@ -1,11 +1,24 @@
-function productCardClick(event) {
-    // Get the image and description of the product card that was clicked.
-    const image = event.target.querySelector('.shop-card-image').src;
-    const description = event.target.querySelector('.shop-card-content').textContent;
+// toma el id 
+const linkElement = document.getElementById('grounded-img');
+
+// agrega un click event listener al id
+linkElement.addEventListener('click', function(event) {
+  event.preventDefault(); // previene de un comportamiento por defecto al seguir el link
+
+  // agrega la accion necesaria a la pagina de destino
+  changeImagesInVerMasVariacionPage();
+});
+
+function changeImagesInVerMasVariacionPage() {
+    // abre la pagna de destino en otra ventana
+    const newWindow = window.open('/pages/vermasVariacion.html');
   
-    // Change the browser's URL to the product detail page.
-    window.location.href = '/vermasVariacion';
+    // espera a que cargue la pagina
+    newWindow.addEventListener('load', function() {
+      // obtiene la imagen por su nombre de clase u otro selector de la nueva ventana
+      const imageElements = newWindow.document.getElementById('imagencentral');
   
-    // Pass the image and description of the product card as parameters to the product detail page.
-    window.history.pushState({ image, description }, 'Product Detail', '/vermasVariacion.html');
-  }
+      // Update the src attribute to change the image
+    imageElement.src = 'assets/images/shop-img-1.jpg';
+});
+}
